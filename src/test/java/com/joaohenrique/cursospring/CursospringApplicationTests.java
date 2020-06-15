7bc327288fb52c760d918efa40e71ca04065a735
João Henrique <johnrick.jh@gmail.com>
2020-06-15 15:52:11 -0300

    Projeto criado

---
+++ src/test/java/com/joaohenrique/cursospring/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaohenrique.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
