0452a5b88734a5d3f25938d60a4077c3abf198dc
João Henrique <johnrick.jh@gmail.com>
2020-06-16 02:06:03 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/joaohenrique/cursospring/CursospringApplication.java
+++ src/main/java/com/joaohenrique/cursospring/CursospringApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.joaohenrique.cursospring.domain.Categoria;
 import com.joaohenrique.cursospring.domain.Cidade;
+import com.joaohenrique.cursospring.domain.Cliente;
+import com.joaohenrique.cursospring.domain.Endereco;
 import com.joaohenrique.cursospring.domain.Estado;
 import com.joaohenrique.cursospring.domain.Produto;
+import com.joaohenrique.cursospring.domain.enums.TipoCliente;
 import com.joaohenrique.cursospring.repositories.CategoriaRepository;
 import com.joaohenrique.cursospring.repositories.CidadeRepository;
+import com.joaohenrique.cursospring.repositories.ClienteRepository;
+import com.joaohenrique.cursospring.repositories.EnderecoRepository;
 import com.joaohenrique.cursospring.repositories.EstadoRepository;
 import com.joaohenrique.cursospring.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursospringApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -42,12 +51,20 @@ public class CursospringApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
-		Estado e1 = new Estado(null, "Minas Gerais");
-		Estado e2 = new Estado(null, "São Paulo");
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
 		
-		Cidade c1 = new Cidade(null, "Uberlândia", e1);
-		Cidade c2 = new Cidade(null, "São Paulo", e2);
-		Cidade c3 = new Cidade(null, "Campinas", e2);
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -56,13 +73,15 @@ public class CursospringApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		e1.getCidades().addAll(Arrays.asList(c1));
-		e2.getCidades().addAll(Arrays.asList(c2,c3));
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-		estadoRepository.saveAll(Arrays.asList(e1,e2));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	}
 
 }
