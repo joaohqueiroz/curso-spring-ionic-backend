a33b4994dc127b1168de6d5ce7922a29eca71d6d
João Henrique <johnrick.jh@gmail.com>
2020-06-17 00:57:02 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/joaohenrique/cursospring/CursospringApplication.java
+++ src/main/java/com/joaohenrique/cursospring/CursospringApplication.java
@@ -1,5 +1,6 @@
 package com.joaohenrique.cursospring;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.joaohenrique.cursospring.domain.Cidade;
 import com.joaohenrique.cursospring.domain.Cliente;
 import com.joaohenrique.cursospring.domain.Endereco;
 import com.joaohenrique.cursospring.domain.Estado;
+import com.joaohenrique.cursospring.domain.Pagamento;
+import com.joaohenrique.cursospring.domain.PagamentoComBoleto;
+import com.joaohenrique.cursospring.domain.PagamentoComCartao;
+import com.joaohenrique.cursospring.domain.Pedido;
 import com.joaohenrique.cursospring.domain.Produto;
+import com.joaohenrique.cursospring.domain.enums.EstadoPagamento;
 import com.joaohenrique.cursospring.domain.enums.TipoCliente;
 import com.joaohenrique.cursospring.repositories.CategoriaRepository;
 import com.joaohenrique.cursospring.repositories.CidadeRepository;
 import com.joaohenrique.cursospring.repositories.ClienteRepository;
 import com.joaohenrique.cursospring.repositories.EnderecoRepository;
 import com.joaohenrique.cursospring.repositories.EstadoRepository;
+import com.joaohenrique.cursospring.repositories.PagamentoRepository;
+import com.joaohenrique.cursospring.repositories.PedidoRepository;
 import com.joaohenrique.cursospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursospringApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -63,8 +75,18 @@ public class CursospringApplication implements CommandLineRunner {
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -82,6 +104,8 @@ public class CursospringApplication implements CommandLineRunner {
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
--- src/main/java/com/joaohenrique/cursospring/domain/Cliente.java
+++ src/main/java/com/joaohenrique/cursospring/domain/Cliente.java
@@ -37,6 +37,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -105,6 +108,14 @@ public class Cliente implements Serializable{
 		this.enderecos = enderecos;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+	
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -129,5 +140,6 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
+
 	
 }
