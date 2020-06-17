f1535e1241b31da0833220e147ae7d6e8644b862
João Henrique <johnrick.jh@gmail.com>
2020-06-17 01:37:12 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/joaohenrique/cursospring/CursospringApplication.java
+++ src/main/java/com/joaohenrique/cursospring/CursospringApplication.java
@@ -13,6 +13,7 @@ import com.joaohenrique.cursospring.domain.Cidade;
 import com.joaohenrique.cursospring.domain.Cliente;
 import com.joaohenrique.cursospring.domain.Endereco;
 import com.joaohenrique.cursospring.domain.Estado;
+import com.joaohenrique.cursospring.domain.ItemPedido;
 import com.joaohenrique.cursospring.domain.Pagamento;
 import com.joaohenrique.cursospring.domain.PagamentoComBoleto;
 import com.joaohenrique.cursospring.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.joaohenrique.cursospring.repositories.CidadeRepository;
 import com.joaohenrique.cursospring.repositories.ClienteRepository;
 import com.joaohenrique.cursospring.repositories.EnderecoRepository;
 import com.joaohenrique.cursospring.repositories.EstadoRepository;
+import com.joaohenrique.cursospring.repositories.ItemPedidoRepository;
 import com.joaohenrique.cursospring.repositories.PagamentoRepository;
 import com.joaohenrique.cursospring.repositories.PedidoRepository;
 import com.joaohenrique.cursospring.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursospringApplication implements CommandLineRunner {
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -84,6 +88,10 @@ public class CursospringApplication implements CommandLineRunner {
 		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
 		ped2.setPagamento(pagto2);
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
@@ -98,6 +106,13 @@ public class CursospringApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
@@ -106,6 +121,7 @@ public class CursospringApplication implements CommandLineRunner {
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
