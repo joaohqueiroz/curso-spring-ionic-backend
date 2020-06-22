97a6a4c75606f2eeffe0d74fc2e667e950ef3e2d
João Henrique <johnrick.jh@gmail.com>
2020-06-22 00:56:46 -0300

    Busca de pedidos por nome e categorias - PARTE 2

---
+++ src/main/java/com/joaohenrique/cursospring/services/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.joaohenrique.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.joaohenrique.cursospring.domain.Categoria;
+import com.joaohenrique.cursospring.domain.Produto;
+import com.joaohenrique.cursospring.repositories.CategoriaRepository;
+import com.joaohenrique.cursospring.repositories.ProdutoRepository;
+import com.joaohenrique.cursospring.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepo.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+	
+}
