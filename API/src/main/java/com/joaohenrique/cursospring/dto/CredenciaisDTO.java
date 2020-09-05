16988806d484aeaba332ba5af4179907c3f11b5a
João Henrique <johnrick.jh@gmail.com>
2020-09-05 17:22:54 -0300

    Implementando autenticação e geração do token JWT

---
+++ API/src/main/java/com/joaohenrique/cursospring/dto/CredenciaisDTO.java
@@ -0,0 +1,30 @@
+package com.joaohenrique.cursospring.dto;
+
+import java.io.Serializable;
+
+public class CredenciaisDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	private String password;
+	
+	public CredenciaisDTO() {
+		
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
