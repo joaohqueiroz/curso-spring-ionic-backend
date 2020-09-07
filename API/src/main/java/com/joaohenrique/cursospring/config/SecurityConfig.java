d0aaf31a76c909cb7f80605ca269554415839ff5
João Henrique <johnrick.jh@gmail.com>
2020-09-07 15:11:17 -0300

    Implementando autorizacao

--- API/src/main/java/com/joaohenrique/cursospring/config/SecurityConfig.java
+++ API/src/main/java/com/joaohenrique/cursospring/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.joaohenrique.cursospring.security.JWTAuthorizationFilter;
 import com.joaohenrique.cursospring.security.JWTUtil;
 import com.joaohenrique.cursospring.services.JWTAuthenticationFilter;
 
@@ -54,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
